84234934c719f37601b4faa9c6e04e297e7ebc59
Ugiagbe Emmanuel <ugiagbeemmanuel1@gmail.com>
2022-07-05 02:51:31 +0100

    Project Update 17

---
+++ precache-manifest.81f8b001fcdfe3462cf78cff55b27beb.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa45f8fefe7631633f2af378b9da6ba2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0cca3f21a93cdd6be99e",
+    "url": "./static/css/2.966e45a1.chunk.css"
+  },
+  {
+    "revision": "661e29614cd2303cc2a6",
+    "url": "./static/css/main.344f1341.chunk.css"
+  },
+  {
+    "revision": "0cca3f21a93cdd6be99e",
+    "url": "./static/js/2.c2c5ff80.chunk.js"
+  },
+  {
+    "revision": "6f14b1fe88e0584e26d5d40640dc906e",
+    "url": "./static/js/2.c2c5ff80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "661e29614cd2303cc2a6",
+    "url": "./static/js/main.a439c09f.chunk.js"
+  },
+  {
+    "revision": "4b559e69254c305f55de",
+    "url": "./static/js/runtime-main.66091f9c.js"
+  },
+  {
+    "revision": "0bc1c1a552b523329fa15d067110f1fe",
+    "url": "./static/media/Cart.0bc1c1a5.svg"
+  },
+  {
+    "revision": "6238ecfdb49a1dd7550ec73db3bf930f",
+    "url": "./static/media/Discover.6238ecfd.svg"
+  },
+  {
+    "revision": "c6bce190beb67dffe188bf261603cc10",
+    "url": "./static/media/Group.c6bce190.svg"
+  },
+  {
+    "revision": "7da654bec18e675d042f1f725433ce5b",
+    "url": "./static/media/Group2.7da654be.svg"
+  },
+  {
+    "revision": "779b93d494cd5177ec2bcde70e2f01a0",
+    "url": "./static/media/Visa.779b93d4.svg"
+  },
+  {
+    "revision": "a570752f4f287e36a8d94913c44ea300",
+    "url": "./static/media/card.a570752f.jpg"
+  },
+  {
+    "revision": "68aeae728a4df1ca2bc03df195e1ebb4",
+    "url": "./static/media/course.68aeae72.svg"
+  },
+  {
+    "revision": "c4f21caf70360dcf655972e1af0ac93c",
+    "url": "./static/media/customer.c4f21caf.svg"
+  },
+  {
+    "revision": "73e6b012bf1e5ab3ba41033aa8f977de",
+    "url": "./static/media/flags_responsive.73e6b012.png"
+  },
+  {
+    "revision": "47d98913c1b369461c4510599afb5001",
+    "url": "./static/media/help.47d98913.svg"
+  },
+  {
+    "revision": "d6683bdb763a33749b27c6db952820bf",
+    "url": "./static/media/ins.d6683bdb.svg"
+  },
+  {
+    "revision": "2de00efe33701dac3a1fe75cc907adeb",
+    "url": "./static/media/invoice.2de00efe.svg"
+  },
+  {
+    "revision": "49e851d728a0aeba3bcc8826a96de386",
+    "url": "./static/media/lead.49e851d7.svg"
+  },
+  {
+    "revision": "9df535bf90e30f52a9b3004fb843f370",
+    "url": "./static/media/logo.9df535bf.png"
+  },
+  {
+    "revision": "e3932c9c268649ec2c8f0f2cafe524ab",
+    "url": "./static/media/logout.e3932c9c.svg"
+  },
+  {
+    "revision": "0f76d40dd64ab3b2a628b6b224af643a",
+    "url": "./static/media/markeet.0f76d40d.svg"
+  },
+  {
+    "revision": "1b32839228929f68ef0a99b1b919e65d",
+    "url": "./static/media/messages.1b328392.svg"
+  },
+  {
+    "revision": "9c4c6214624da99995d1c4814842d145",
+    "url": "./static/media/orders.9c4c6214.svg"
+  },
+  {
+    "revision": "121254f73060bcbb53ca13258dbd134f",
+    "url": "./static/media/primeicons.121254f7.ttf"
+  },
+  {
+    "revision": "25954ab5e32c01d577dcafd035597f5d",
+    "url": "./static/media/primeicons.25954ab5.svg"
+  },
+  {
+    "revision": "3b6e3706f42d8876fe364ab4d75683fd",
+    "url": "./static/media/primeicons.3b6e3706.woff"
+  },
+  {
+    "revision": "b0f5d02f4e70967dcc8162e938484366",
+    "url": "./static/media/primeicons.b0f5d02f.eot"
+  },
+  {
+    "revision": "24b5d2b8bf5c4d83f69149a35b605aa8",
+    "url": "./static/media/question.24b5d2b8.svg"
+  },
+  {
+    "revision": "ddc361d14b58561ebdc018f41c275b1f",
+    "url": "./static/media/reports.ddc361d1.svg"
+  },
+  {
+    "revision": "82ee5b0889b7e229a6d837125556435c",
+    "url": "./static/media/setting.82ee5b08.svg"
+  },
+  {
+    "revision": "8ad85162fa77ababf7bf1f4ce76eea02",
+    "url": "./static/media/staf.8ad85162.svg"
+  },
+  {
+    "revision": "9189d175b0a1e2e3e48343f558dc319c",
+    "url": "./static/media/ticket.9189d175.svg"
+  },
+  {
+    "revision": "78e6dd463045e98c7d3c9f6227d47298",
+    "url": "./static/media/tips.78e6dd46.svg"
+  }
+]);
